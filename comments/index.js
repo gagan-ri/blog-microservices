de1d5b34398e5043f28fe1ce0aa20d5cecb30c54
gagan-ri <96907707+gagan-ri@users.noreply.github.com>
2022-01-05 18:37:50 +0530

    Added event bus

--- comments/index.js
+++ comments/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
+var cors = require('cors');
+const axios = require('axios');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const commentsByPostId = {} ;
@@ -10,17 +13,31 @@ app.get('/posts/:id/comments', (req,res)=>{
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req,res)=>{
+app.post('/posts/:id/comments', async (req,res)=>{
     const commentId = randomBytes(4).toString('hex');
 
     const { content } = req.body ;
     const comments = commentsByPostId[req.params.id] || [] ;
 
-    comments.push({ id: commentId, content });
+    comments.push({ id: commentId, content, status: 'pending' });
 
     commentsByPostId[req.params.id] = comments ;
 
+    await axios.post('http://localhost:4005/events',{
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id,
+            status: 'pending'
+        }
+    })
+
     res.status(201).send(comments); 
+});
+
+app.post('/events',(req,res)=>{
+    res.status(201).send();
 });
 
 app.listen(4001, ()=> console.log('Listening on 4001'));
